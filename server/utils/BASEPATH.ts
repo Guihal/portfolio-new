591e6adeead79d9e2cd5883e6f4e8f404eaabf55
Guihal <guihalbonik@gmail.com>
2026-03-16 19:36:40 +0300

    изменения

--- server/utils/BASEPATH.ts
+++ server/utils/BASEPATH.ts
@@ -3,12 +3,12 @@ import { existsSync } from 'node:fs';
 
 function getBasePath() {
     // Продакшн (Vercel) — .output/server/assets/entry
-    const prodPath = join(process.cwd(), '.output/server/assets/entry');
+    const prodPath = join(process.cwd(), 'public/entry');
     console.log('Проверяем продакшн путь:', prodPath);
     if (existsSync(prodPath)) return prodPath;
 
     // Dev — server/assets/entry (относительно корня проекта)
-    return join(process.cwd(), 'server/assets/entry');
+    return join(process.cwd(), 'public/entry');
 }
 
 export const BASEPATH = getBasePath();
