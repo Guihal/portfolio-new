084a6d31214449d5b8999a9c6e39a9aa992fdc41
Guihal <guihalbonik@gmail.com>
2026-03-16 19:43:33 +0300

    jgznm

--- server/utils/BASEPATH.ts
+++ server/utils/BASEPATH.ts
@@ -1,13 +1,12 @@
 import { join } from 'path';
-import { existsSync } from 'node:fs';
 
 function getBasePath() {
-    const prodPath = join(process.cwd(), 'entry');
-    console.log('Проверяем продакшн путь:', prodPath);
-    if (existsSync(prodPath)) return prodPath;
-
-    // Dev — server/assets/entry (относительно корня проекта)
-    return join(process.cwd(), 'public/entry');
+    if (!import.meta.dev) {
+        const prodPath = join(process.cwd(), 'entry');
+        return prodPath;
+    } else {
+        return join(process.cwd(), 'public/entry');
+    }
 }
 
 export const BASEPATH = getBasePath();
